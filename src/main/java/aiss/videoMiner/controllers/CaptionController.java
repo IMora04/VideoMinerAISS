503dc823750f39f525e05f619923e4dbd321437d
Elena <eleferrei@alum.us.es>
2024-05-12 19:15:11 +0200

    Caption documentation added

--- src/main/java/aiss/videoMiner/controllers/CaptionController.java
+++ src/main/java/aiss/videoMiner/controllers/CaptionController.java
@@ -2,6 +2,12 @@ package aiss.videoMiner.controllers;
 
 import aiss.videoMiner.model.Caption;
 import aiss.videoMiner.repository.CaptionRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Captions", description = "Captions management API")
+
 @RestController
 @RequestMapping("/videominer/captions")
 public class CaptionController {
@@ -17,10 +25,33 @@ public class CaptionController {
     @Autowired
     CaptionRepository captionRepository;
 
+    @Operation(
+            summary = "Retrive a list of captions",
+            description = "Get a list of captions",
+            tags = {"captions", "get"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "List of captions", content = {@Content(schema = @Schema(implementation = Caption.class),
+                    mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Captions not found", content = {@Content(schema = @Schema())})
+    })
+
     @GetMapping
     public List<Caption> findAll(){
         return captionRepository.findAll();
     }
+
+    @Operation(
+            summary = "Retrive a captions by id",
+            description = "Get a captions specifying its id",
+            tags = {"captions", "get"}
+    )
+
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = Caption.class),
+                    mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema())})
+    })
 
     @GetMapping("/{id}")
     public Caption findOne(@PathVariable String id){
